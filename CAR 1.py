9e82c7f93a83476306d301980c949edaec0c02bb
O3iz <95961462+O3iz@users.noreply.github.com>
2022-03-03 15:47:20 +0800

    Update CAR 1.py

--- CAR 1.py
+++ CAR 1.py
@@ -9,15 +9,7 @@ def init():
  gpio.setup(24, gpio.OUT)
 
 def forward(sec):
- init()
- gpio.output(17, True)
- gpio.output(22, False)
- gpio.output(23, True) 
- gpio.output(24, False)
- time.sleep(sec)
- gpio.cleanup()
-
-def reverse(sec):
+ 
  init()
  gpio.output(17, False)
  gpio.output(22, True)
@@ -25,8 +17,40 @@ def reverse(sec):
  gpio.output(24, True)
  time.sleep(sec)
  gpio.cleanup()
+ 
+def reverse(sec):
+ init()
+ gpio.output(17, True)
+ gpio.output(22, False)
+ gpio.output(23, True) 
+ gpio.output(24, False)
+ time.sleep(sec)
+ gpio.cleanup()
+ 
+ def left(sec):
+ gpio.output(17, True)
+ gpio.output(22, False)
+ gpio.output(23, False) 
+ gpio.output(24, True)
+ time.sleep(sec)
+ gpio.cleanup()
+ 
+ def right(sec):
+ gpio.output(17, False)
+ gpio.output(22, True)
+ gpio.output(23, True) 
+ gpio.output(24, False)
+ time.sleep(sec)
+ gpio.cleanup()
+ 
+  
+
 
 print "forward"
-forward(4)
+forward(2)
 print "reverse"
 reverse(2)
+print ("left")
+left(2)
+print ("right")
+right(2)
