db2be4d4ea3eea1b8a41b37b06baf55190befe99
O3iz <95961462+O3iz@users.noreply.github.com>
2022-03-03 14:53:59 +0800

    Update CAR 1.py

--- CAR 1.py
+++ CAR 1.py
@@ -1,56 +1,82 @@
-import RPi.GPIO as gpio
-import time
+import RPi.GPIO as GPIO          
+from time import sleep
 
-in1 = 17
-in2 = 22
-in3 = 23
-in4 = 24
+in1 = 24
+in2 = 23
+en = 25
+temp1=1
 
-def init():
-    gpio.setmode(gpio.BCM)
-    gpio.setup(in1, gpio.OUT)
-    gpio.setup(in2, gpio.OUT)
-    gpio.setup(in3, gpio.OUT)
-    gpio.setup(in4, gpio.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+p=GPIO.PWM(en,1000)
+p.start(25)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")    
 
-def forward(sec):
-    init()
-    gpio.output(in1, True)
-    gpio.output(in2, False)
-    gpio.output(in3, True) 
-    gpio.output(in4, False)
-    time.sleep(sec)
-    gpio.cleanup()
+while(1):
 
-def reverse(sec):
-    init()
-    gpio.output(in1, False)
-    gpio.output(in2, True)
-    gpio.output(in3, False) 
-    gpio.output(in4, True)
-    time.sleep(sec)
-    gpio.cleanup()
-
-def right(sec):
-    gpio.output(in1, False)
-    gpio.output(in2, True)
-    gpio.output(in3, False) 
-    gpio.output(in4, True)
-    time.sleep(sec)
-    gpio.cleanup()
+    x=raw_input()
     
- def left(sec):
-    gpio.output(in1, False)
-    gpio.output(in2, True)
-    gpio.output(in3, False) 
-    gpio.output(in4, True)
-    time.sleep(sec)
-    gpio.cleanup()
-print "forward"
-forward(3)
-print "reverse"
-reverse(3)
-print "left"
-left(3)
-print "right"
-right(3)
+    if x=='r':
+        print("run")
+        if(temp1==1):
+         GPIO.output(in1,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+         print("forward")
+         x='z'
+        else:
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+         print("backward")
+         x='z'
+
+
+    elif x=='s':
+        print("stop")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        x='z'
+
+    elif x=='f':
+        print("forward")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        x='z'
+
+    elif x=='b':
+        print("backward")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1=0
+        x='z'
+
+    elif x=='l':
+        print("low")
+        p.ChangeDutyCycle(25)
+        x='z'
+
+    elif x=='m':
+        print("medium")
+        p.ChangeDutyCycle(50)
+        x='z'
+
+    elif x=='h':
+        print("high")
+        p.ChangeDutyCycle(75)
+        x='z'
+     
+    
+    elif x=='e':
+        GPIO.cleanup()
+        break
+    
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
