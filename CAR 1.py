4a9a19d6e8e695d48f77561ed1bd09cda9341c89
O3iz <95961462+O3iz@users.noreply.github.com>
2022-05-04 04:37:42 +0800

    Update CAR 1.py

--- CAR 1.py
+++ CAR 1.py
@@ -1,15 +1,12 @@
 import RPi.GPIO as gpio
 import time
-
 def init():
  gpio.setmode(gpio.BCM)
  gpio.setup(17, gpio.OUT)
  gpio.setup(22, gpio.OUT)
  gpio.setup(23, gpio.OUT)
  gpio.setup(24, gpio.OUT)
-
 def forward(sec):
- 
  init()
  gpio.output(17, False)
  gpio.output(22, True)
@@ -17,7 +14,6 @@ def forward(sec):
  gpio.output(24, True)
  time.sleep(sec)
  gpio.cleanup()
- 
 def reverse(sec):
  init()
  gpio.output(17, True)
@@ -26,26 +22,20 @@ def reverse(sec):
  gpio.output(24, False)
  time.sleep(sec)
  gpio.cleanup()
- 
- def left(sec):
+def left(sec):
  gpio.output(17, True)
  gpio.output(22, False)
  gpio.output(23, False) 
  gpio.output(24, True)
  time.sleep(sec)
  gpio.cleanup()
- 
- def right(sec):
+def right(sec):
  gpio.output(17, False)
  gpio.output(22, True)
  gpio.output(23, True) 
  gpio.output(24, False)
  time.sleep(sec)
  gpio.cleanup()
- 
-  
-
-
 print "forward"
 forward(2)
 print "reverse"
